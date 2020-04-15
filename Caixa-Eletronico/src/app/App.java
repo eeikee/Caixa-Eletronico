e9e61c25146105b231a9c753ff101974893df488
eeikee <konumaeike5618@hotmail.com>
2020-04-14 23:39:20 -0300

    Updated the algorithm and implamented the menu area

--- Caixa-Eletronico/src/app/App.java
+++ Caixa-Eletronico/src/app/App.java
@@ -72,10 +72,10 @@ public class App {
       final String login1 = "12345678900";
       final String login2 = "123.456.789-00";
       final String sn = "01020304";
-      String cpf, passw;
-      int try1 = 3, opt;
+      String cpf, passw,opt2;
+      int try1 = 3, opt = 0;
       double saldo = 1000.00;
-        // iniciar();
+        iniciar();
        System.out.print("\033\143");
        System.out.printf("%c",9487);
        for(int y = 0; y < 31; y++){
@@ -113,33 +113,60 @@ public class App {
             passw = Input.next();
             try1--;
            }while(passw.equals(sn) == false);
-           System.out.print("\033\143");
-           System.out.printf("%c",9487);
-            for(int y = 0; y < 31; y++){
-                System.out.printf("%c",9473);               
-                }
-            System.out.printf("%c",9491);  
-            System.out.printf("\n%c    Caixa Eletronico - Menu    %c",9475,9475);
-            System.out.printf("\n%c    1 – Saldo                  %c",9475,9475);
-            System.out.printf("\n%c    2 – Depósito               %c",9475,9475);
-            System.out.printf("\n%c    3 – Saque                  %c",9475,9475);
-            System.out.printf("\n%c    0 – Sair                   %c",9475,9475);
-            System.out.printf("\n%c Selecione uma opcao:          %c",9475,9475);
-            System.out.printf("\b\b\b\b\b\b\b\b\b\b\b");
-            opt = Input.nextInt();
-            switch(opt){
-                case 1: System.out.printf("\n%c Selecione uma opcao:          %c",9475,9475);
-                break;
+           do{
+           do{
+            System.out.print("\033\143");
+            System.out.printf("%c",9487);
+             for(int y = 0; y < 31; y++){
+                 System.out.printf("%c",9473);               
+                 }
+             System.out.printf("%c",9491);  
+             System.out.printf("\n%c    Menu - Caixa Eletronico    %c",9475,9475);
+             System.out.printf("\n%c    1 – Saldo                  %c",9475,9475);
+             System.out.printf("\n%c    2 – Depósito               %c",9475,9475);
+             System.out.printf("\n%c    3 – Saque                  %c",9475,9475);
+             System.out.printf("\n%c    0 – Sair                   %c",9475,9475);
+             System.out.printf("\n%c    Selecione uma opcao:       %c",9475,9475);
+             System.out.printf("\b\b\b\b\b\b\b");
+             opt = Input.nextInt();
+             switch(opt){
+                case 1: System.out.printf("\n%c    Saldo atual: %f              %c",9475,saldo,9475);
                 case 2: System.out.printf("\n%c Selecione uma opcao:          %c",9475,9475);
                 break;
                 case 3: System.out.printf("\n%c Selecione uma opcao:          %c",9475,9475);
                 break;
-                case 0: System.out.printf("\n%c Selecione uma opcao:          %c",9475,9475);
-                break;
-                default: System.out.printf("\n%c      *Opcao invalida*        %c",9475,9475);
-                System.out.printf("\n%c        *Programa finalizado*          %c",9475,9475);
+                case 0: System.out.printf("%c ------------------------------%c",9475,9475); 
+                System.out.printf("\n%c           Ate logo!           %c",9475,9475);
+                System.out.printf("\n%c",9495);
+                 for(int i = 0; i < 31; i++){
+                     System.out.printf("%c",9473);               
+                     }
+                 System.out.printf("%c\n",9499);
+                Thread.sleep(2000);
                 System.exit(0);
+                default: System.out.printf("%c ------------------------------%c",9475,9475); 
+                System.out.printf("\n%c      *Opcao invalida*         %c",9475,9475);
+                System.out.printf("\n%c",9495);
+                 for(int i = 0; i < 31; i++){
+                     System.out.printf("%c",9473);               
+                     }
+                 System.out.printf("%c",9499);
+                Thread.sleep(1000);
             }
+           }while(opt != 0 || opt != 1 || opt != 2 || opt != 3);
+           System.out.printf("\n%c ------------------------------%c",9475,9475); 
+           System.out.printf("\n%c Deseja continuar(Y/N):        %c",9475,9475); 
+           opt2 = Input.next();
+        }while(opt2.equals("Y") == true || opt2.equals("y") == true);
+        System.out.printf("%c ------------------------------%c",9475,9475); 
+                System.out.printf("\n%c           Ate logo!           %c",9475,9475);
+                System.out.printf("\n%c",9495);
+                 for(int i = 0; i < 31; i++){
+                     System.out.printf("%c",9473);               
+                     }
+                 System.out.printf("%c\n",9499);
+                Thread.sleep(2000);
+                System.exit(0);
             }
       else{
         System.out.printf("\r%c *Tela de acesso nao permitido*%c",9475,9475);
